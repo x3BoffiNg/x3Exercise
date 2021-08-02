4efb852e84acd9db05024585359efd8a62b3459d
x3BoffiNg <85842044+x3BoffiNg@users.noreply.github.com>
2021-08-02 15:48:45 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ const date = new Date();
 const renderCalendar = () => {
   date.setDate(1);
 
-  const monthDays = document.querySelector(".days");
+  const monthDays = document.querySelector(".days"); //
 
   const lastDay = new Date(
     date.getFullYear(),
-    date.getMonth() + 1,
+    date.getMonth() + 1, //get last date of current month so getmonth() +1//
     0
   ).getDate();
 
-  const prevLastDay = new Date(
+  const prevLastDay = new Date( //get last date of previous month 0 //
     date.getFullYear(),
     date.getMonth(),
     0
@@ -25,7 +25,7 @@ const renderCalendar = () => {
     0
   ).getDay();
 
-  const nextDays = 7 - lastDayIndex - 1;
+  const nextDays = 7 - lastDayIndex - 1; //to diplay the day of next month deduct 7 and 1
 
   const months = [
     "January",
@@ -42,41 +42,41 @@ const renderCalendar = () => {
     "December",
   ];
 
-  document.querySelector(".date h1").innerHTML = months[date.getMonth()];
+  document.querySelector(".date h1").innerHTML = months[date.getMonth()]; //needd get month if get index just one month can be shown//
 
-  document.querySelector(".date p").innerHTML = new Date().toDateString();
+  document.querySelector(".date p").innerHTML = new Date().toDateString(); //display day dynamically//
 
-  let days = "";
+  let days = ""; //empty string//
 
-  for (let x = firstDayIndex; x > 0; x--) {
-    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
+  for (let x = firstDayIndex; x > 0; x--) {   //ITERATE THE DAYS MON,TUES... each iteration create a new div for prev month date //
+    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`; //+1 or else may will have 29 no 30//
   }
 
-  for (let i = 1; i <= lastDay; i++) {
-    if (
+  for (let i = 1; i <= lastDay; i++) { //
+    if (  //highlight current date - if compare counter1 to countdate with match they will add a div element//
       i === new Date().getDate() &&
       date.getMonth() === new Date().getMonth()
     ) {
-      days += `<div class="today">${i}</div>`;
+      days += `<div class="today">${i}</div>`; //content is the i variable//
     } else {
       days += `<div>${i}</div>`;
     }
   }
 
-  for (let j = 1; j <= nextDays; j++) {
+  for (let j = 1; j <= nextDays; j++) { //j=1 because each month start with 1,each iteration need to create new div aand pass them as the counter of dsys insert variable j
     days += `<div class="next-date">${j}</div>`;
-    monthDays.innerHTML = days;
+    monthDays.innerHTML = days; //number of the days //
   }
 };
 
-document.querySelector(".prev").addEventListener("click", () => {
+document.querySelector(".prev").addEventListener("click", () => { //the left arrow attach to event llistener vent is click and call the function//
   date.setMonth(date.getMonth() - 1);
-  renderCalendar();
+  renderCalendar(); //need to call rendercalendar functiion for it to work//
 });
 
-document.querySelector(".next").addEventListener("click", () => {
+document.querySelector(".next").addEventListener("click", () => {{ //the right arrow attach to event llistener vent is click and call the function//
   date.setMonth(date.getMonth() + 1);
-  renderCalendar();
+  renderCalendar();  renderCalendar(); //need to call rendercalendar functiion for it to work//
 });
 
-renderCalendar();
+renderCalendar();  renderCalendar(); //need to call rendercalendar functiion global scope for it to work//
